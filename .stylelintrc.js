857f8ce43e559d5d04540bf66ac12581b9d5b2d4
tkrauo <tkr00st@gmail.com>
2020-07-01 16:58:55 +0900

    stylelint、eslint、prettierの設定のベースを作成

---
+++ .stylelintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "extends": [
+    "stylelint-config-recommended-scss",
+    "stylelint-config-standard",
+    "stylelint-prettier/recommended"
+  ]
+};
---
+++ sample/js/script.js
@@ -0,0 +1,3 @@
+console.log("log");
+
+const hhheee = 'test';
